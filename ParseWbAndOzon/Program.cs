891acb0cd23a16f0806d2695277d4855829daf5f
by_Onyx <ilyayaldinov@gmail.com>
2023-09-19 11:17:17 +0300

    Get first 10 product

---
+++ ParseWbAndOzon/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+
+namespace ParseWbAndOzon;
+
+class Program
+{
+    public static void Main()
+    {
+        FirefoxOptions options = new FirefoxOptions();
+        var driver = new FirefoxDriver(options);
+        try {
+            driver.Navigate().GoToUrl("https://www.wildberries.ru/catalog/0/search.aspx?search=msi");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            /*driver.ExecuteScript(
+                "window.scrollTo(0,document.body.scrollHeight)");
+
+            Thread.Sleep(3000);*/
+            
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            var price = driver.FindElements(By.TagName("article"));
+            
+            Console.WriteLine(price.Count);
+            foreach (var webElement in price)
+            {
+                Console.WriteLine(webElement.Text);
+            }
+        } finally {
+            driver.Quit();
+        }
+    } 
+}
