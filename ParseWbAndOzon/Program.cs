5cd1380125976303f07461f5f1241c49e3ca06a4
reedkappa <goshaidze03@gmail.com>
2023-09-19 14:34:24 +0500

    Added scrolling

--- ParseWbAndOzon/Program.cs
+++ ParseWbAndOzon/Program.cs
@@ -13,25 +13,36 @@ class Program
     {
         FirefoxOptions options = new FirefoxOptions();
         var driver = new FirefoxDriver(options);
-        try {
+        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        try
+        {
             driver.Navigate().GoToUrl("https://www.wildberries.ru/catalog/0/search.aspx?search=msi");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-            /*driver.ExecuteScript(
-                "window.scrollTo(0,document.body.scrollHeight)");
+            driver.Manage().Window.Maximize();
+            int y = 50;
+            for (int timer = 0; timer < 19; timer++)
+            {
+                js.ExecuteScript($"window.scrollBy(0,{y.ToString()})");
+                y += 50;
+                Thread.Sleep(1000);
+            }
 
-            Thread.Sleep(3000);*/
-            
+            Thread.Sleep(2000);
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             var price = driver.FindElements(By.TagName("article"));
-            
+
+            Console.Clear();
             Console.WriteLine(price.Count);
             foreach (var webElement in price)
             {
                 Console.WriteLine(webElement.Text);
             }
-        } finally {
+        }
+        finally
+        {
             driver.Quit();
         }
-    } 
+    }
 }
