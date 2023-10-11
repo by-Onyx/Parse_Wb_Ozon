8df64e6eb1d92d2a79252953b5d441ca6cc6c2c2
reedkappa <goshaidze03@gmail.com>
2023-10-12 00:41:13 +0500

    Added getting a new link and surfing pages, need to work with timing

--- ParseWbAndOzon/Parsers/OzonParser.cs
+++ ParseWbAndOzon/Parsers/OzonParser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Globalization;
+using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -14,6 +15,9 @@ public class OzonParser : Parser
     private string productPricesCard;
     private int currentPageNumber;
     private Func<string> GetHandleLink;
+    private string _normalLink;
+    private int _pageNumberIndex;
+    private bool _pastFirstPage = false; //На случай, если на первой странице вылетела капча
     public OzonParser(FirefoxDriver driver, FirefoxOptions options, string productName) : base(driver, options, productName)
     {
         currentPageNumber = 1;
@@ -49,14 +53,28 @@ public class OzonParser : Parser
             driver.Manage().Cookies.DeleteAllCookies();
             button.Click();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            string newLink = driver.Url.Replace("from_global=true", $"from_global=true&page={currentPageNumber}"); //TODO Засунуть newLink в Func GetHandleLink()
+            if (handleLink.Contains("from_global"))
+            {
+                _normalLink = driver.Url.Replace("from_global=true", $"from_global=true&page={currentPageNumber}"); //TODO Засунуть newLink в Func GetHandleLink()
+            }
+            else
+            {
+                _normalLink = driver.Url.Replace("deny_category_prediction=true", $"deny_category_prediction=true&page={currentPageNumber}");
+            }
+            _pageNumberIndex = _normalLink.IndexOf('1', 50);
             i++;
             
             return;
         }
-        
+
+        _pastFirstPage = true;
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append(_normalLink);
+        sb.Remove(_pageNumberIndex, 1);
+        sb.Insert(_pageNumberIndex, $"{currentPageNumber}");
         driver.Manage().Cookies.DeleteAllCookies();
-        driver.Navigate().GoToUrl(GetHandleLink());
+        driver.Navigate().GoToUrl(sb.ToString());
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
     }
     
@@ -78,11 +96,23 @@ public class OzonParser : Parser
         do
         {
             NavigateToPage();
+
+            try
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                var catalogCheck = driver.FindElement(By.CssSelector("#paginatorContent"));
+            }
+            catch(Exception e)
+            {
+                NewDriverConnection();
+            }
             
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             var catalog = driver.FindElement(By.CssSelector("#paginatorContent"));
             if (!CheckBrandTag(catalog))
             {
                 Console.WriteLine("fuck");
+                currentPageNumber++;
                 continue;
             }
             
@@ -90,20 +120,32 @@ public class OzonParser : Parser
             
             ScrollToPageEnd(50);
             
-            // var productCardAttribute =
-            //     catalog.FindElement(By.XPath("//*[@id=\"paginatorContent\"]/div/div/div[1]/div[1]"))
-            //         .GetAttribute("class")
-            //         .Replace(" ", ".");
-            //
-            // /*productPricesCard =
-            //     catalog.FindElement(By.CssSelector($"#paginatorContent > div > div > div:nth-child(1) > div.{productCardAttribute} > div:nth-child(1) > div"))
-            //         .GetAttribute("class")
-            //         .Replace(" ", ".");*/
-            //
-            // Products.AddRange(ProductToModel(catalog.FindElements(By.CssSelector($".{productCardAttribute}"))));
+            var productCardAttribute =
+                catalog.FindElement(By.XPath("//*[@id=\"paginatorContent\"]/div/div/div[1]/div[1]"))
+                    .GetAttribute("class")
+                    .Replace(" ", ".");
+            
+            /*productPricesCard =
+                catalog.FindElement(By.CssSelector($"#paginatorContent > div > div > div:nth-child(1) > div.{productCardAttribute} > div:nth-child(1) > div"))
+                    .GetAttribute("class")
+                    .Replace(" ", ".");*/
+            
+            //Products.AddRange(ProductToModel(catalog.FindElements(By.CssSelector($".{productCardAttribute}"))));
 
             GC.Collect();
         } while (CheckNextPage());
+    }
+
+    private void NewDriverConnection()
+    {
+        driver.Quit();
+        driver = new FirefoxDriver(_options);
+        driver.Manage().Cookies.DeleteAllCookies();
+        if (!_pastFirstPage)
+        {
+            i = 0;
+        }
+        NavigateToPage();
     }
     
     protected override List<ProductModel> ProductToModel(ReadOnlyCollection<IWebElement> elements)
